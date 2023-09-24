00585469f5d6e1f6c091fe56589546e9cadb1f10
subratswain-cpp <cppwithsubrat@gmail.com>
2023-09-24 10:41:24 +0530

    First C++ Program

---
+++ firstcpp.cpp
@@ -0,0 +1,19 @@
+// Write something on screen. Name
+
+// "I am Subrat".
+
+// What command we have to use for writing something on the screen
+
+
+/// College Library ====> C++ Library
+
+// cout => console output => For printing anything on the screen
+
+// # Minimal C++
+#include <iostream> // Header for cout
+
+int main() // COMMENT: 
+{
+    std::cout << "I am Subrat\n"; // \n is escape sequence. Use for newline
+    std::cout << "My Address is Pune";
+}
