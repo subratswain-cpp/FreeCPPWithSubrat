13e29d804a1545abe4f42a8699cf7e81f442783a
subratswain-cpp <cppwithsubrat@gmail.com>
2023-10-05 21:20:18 +0530

    Day2:Add 2 Numbers

---
+++ add_2_nos.cpp
@@ -0,0 +1,23 @@
+/* Add 2 nos. */
+// for comment. Comment is for developers.
+
+// I am Subrat => NO-Not correct english
+// I am Subrat. =>  correct english
+// . in english is ; in C/C++ programming
+// ; => Statement terminator.
+
+// Declare variables before use.
+
+#include <iostream>
+
+int main() {
+  int a;
+  int b;
+  int sum;
+
+  a = 100;
+  b = 2000;
+  sum = a + b;
+  std::cout << "Sum: ";
+  std::cout << sum << '\n'; // CAUTION: Don't keep variable name within double quote!
+}
